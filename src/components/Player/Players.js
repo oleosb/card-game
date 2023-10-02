aff2c6b4884c512bbfd623c5780a35979e0cef1b
oleosb <oleosbo@gmail.com>
2023-10-02 20:20:32 -0300

    Modifying PlayersComponent

--- src/components/Player/Players.js
+++ src/components/Player/Players.js
@@ -1,9 +1,9 @@
 import Player from "./Player";
 import styles from "./Players.module.css";
-import Plus from "../../imgs/plus-solid.svg";
 import Title from "../Helpers/Title";
 import Button from "../Helpers/Button";
 import { ReactComponent as Thumb } from "../../imgs/thumbs-up-solid.svg";
+import { ReactComponent as Plus } from "../../imgs/plus-solid.svg";
 
 const Players = () => {
   return (
@@ -12,12 +12,14 @@ const Players = () => {
       <Player />
       <Player />
       <Player />
-      <button className={styles.addPlayer}>
-        <img src={Plus} alt="Adicionar jogador" />
-      </button>
-      <Button Svg={Thumb} dark={true}>
-        Ok!
-      </Button>
+      <footer>
+        <Button Svg={Plus} dark={true}>
+          Adicionar
+        </Button>
+        <Button Svg={Thumb} dark={true}>
+          Ok
+        </Button>
+      </footer>
     </section>
   );
 };
