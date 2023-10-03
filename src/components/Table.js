04c64ee5838b421c94d249c29e23572f3053c107
oleosb <oleosbo@gmail.com>
2023-10-03 20:05:38 -0300

    Apagando fetch/loop antigo

--- src/components/Table.js
+++ src/components/Table.js
@@ -30,7 +30,7 @@ const Table = () => {
 
   function fetchCards() {
     if (players.length < 2) {
-      setError(true)
+      setError(true);
       setTimeout(() => setError(false), 2000);
     } else {
       setError(false);
@@ -38,58 +38,6 @@ const Table = () => {
     }
   }
 
-  /*
-  const endPoint = `https://www.deckofcardsapi.com/api/deck/new/draw/?count=${36}`;
-  const [tableCards, setTableCards] = useState([]);
-
-  useEffect(() => {
-    fetch(endPoint, {
-      method: "GET",
-      headers: {
-        "Content-Type": "aplication/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setTableCards(data);
-      })
-      .catch((err) => console.log(err));
-  }, [endPoint]);
-
-  if (tableCards.success === true) {
-    return (
-      <div className={styles.table}>
-        {(() => {
-          let rows = [];
-          let n = 5;
-          let cardC = [];
-
-          for (let i = 1; i <= n; i++) {
-            rows.push(
-              <div key={i} data="row">
-                {(() => {
-                  let columns = [];
-                  for (let j = 1; j <= 2 * n - 1; j++) {
-                    if (j >= n - (i - 1) && j <= n + (i - 1)) {
-                      columns.push(
-                        <Card
-                          src={tableCards.cards[cardC.length].image}
-                          key={j}
-                        />
-                      );
-                      cardC.push("+1");
-                    }
-                  }
-                  return columns;
-                })()}
-              </div>
-            );
-          }
-          return rows;
-        })()}
-      </div>
-    );
-  }*/
   return (
     <div className={styles.table}>
       <div className={styles.header}>
